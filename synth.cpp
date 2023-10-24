73e76afed4e8f46e452ae84e1a7aad4a7476e334
SKYWINDTECH\alexandru.radu <alexandru.radu@skywindgroup.com>
2023-10-24 14:56:26 +0300

    Implemented parameter

--- synth.cpp
+++ synth.cpp
@@ -9,78 +9,66 @@
 #define HARMONICS_AMPLITUDE_MIN 0.0
 #define HARMONICS_AMPLITUDE_INCREMENT 0.02
 
-
 struct Parameter {
-    double current_value;
-    double target_value;
+    double current_value = 0.0;
+    double target_value = 0.0;
 
-    double change_time_start;
+    double change_time_start = 0.0;
 
-    const double min_value;
-    const double max_value;
-    const double increment_value;
+    const double min_value = 0.0;
+    const double max_value = 1.0;
+    const double increment_value = 0.05;
 };
 
+const double freq[HARMONICS_COUNT] = {32.7, 65.4, 98.1, 130.8, 163.5, 196.2, 228.9, 261.6, 294.3};
 
-
-
-const double freq[HARMONICS_COUNT] = {32.7,65.4,98.1,130.8,163.5,196.2,228.9,261.6,294.3};
-
-double harmonics_amplitude[HARMONICS_COUNT] = {0};
-double target_harmonics_amplitude[HARMONICS_COUNT] = {0};
-double change_start_time_harmonics_amplitude[HARMONICS_COUNT] = {0};
-
+Parameter harmonics_amplitude[HARMONICS_COUNT] = {Parameter()};
 
 double detune = 0.0;
 double phase = 0.0;
 
 double synth_time = 0;
-double parameter_change_duration = 100; //ms
+double parameter_change_duration = 100; // ms
 
-
-
-void increase_harmonics_amplitude(const int harmonics_index)
+void increase_value(Parameter &parameter)
 {
-    change_start_time_harmonics_amplitude[harmonics_index] = synth_time;
+    parameter.change_time_start = synth_time;
 
-    if (target_harmonics_amplitude[harmonics_index] < HARMONICS_AMPLITUDE_MAX)
-        target_harmonics_amplitude[harmonics_index] += HARMONICS_AMPLITUDE_INCREMENT;
+    if (parameter.target_value < parameter.max_value)
+        parameter.target_value += parameter.increment_value;
 }
 
-void decrease_harmonics_amplitude(const int harmonics_index)
+void decrease_value(Parameter &parameter)
 {
-    change_start_time_harmonics_amplitude[harmonics_index] = synth_time;
+    parameter.change_time_start = synth_time;
 
-    if (target_harmonics_amplitude[harmonics_index] > HARMONICS_AMPLITUDE_MIN)
-        target_harmonics_amplitude[harmonics_index] -= HARMONICS_AMPLITUDE_INCREMENT;
+    if (parameter.target_value > parameter.min_value)
+        parameter.target_value -= parameter.increment_value;
 
-    // some precision problem
-    if (target_harmonics_amplitude[harmonics_index] < HARMONICS_AMPLITUDE_INCREMENT)
-        target_harmonics_amplitude[harmonics_index] = HARMONICS_AMPLITUDE_MIN;
+    if (parameter.target_value < parameter.increment_value)
+        parameter.target_value = parameter.min_value;
 }
 
-void update_harmonics_amplitude(const int harmonics_index)
+void update_parameter(Parameter &parameter)
 {
-    const double &target = target_harmonics_amplitude[harmonics_index];
-    double &current = harmonics_amplitude[harmonics_index];
-
-    if (target != current)
+    if (parameter.target_value != parameter.current_value)
     {
-        double dt = synth_time - change_start_time_harmonics_amplitude[harmonics_index];
-        double step_value = pow(synth_time - change_start_time_harmonics_amplitude[harmonics_index], 2.0);
+        double step_value = pow(synth_time - parameter.change_time_start, 2.0);
 
-        if (target < current)
+        if (parameter.target_value < parameter.current_value)
         {
-            current -= step_value;
+            parameter.current_value -= step_value;
 
-            if (current < target) current = target;
+            if (parameter.current_value < parameter.target_value)
+                parameter.current_value = parameter.target_value;
         }
 
-        if (target > current)
+        if (parameter.target_value > parameter.current_value)
         {
-            current += step_value;
+            parameter.current_value += step_value;
 
-            if (current > target) current = target;
+            if (parameter.current_value > parameter.target_value)
+                parameter.current_value = parameter.target_value;
         }
     }
 }
@@ -96,9 +84,9 @@ void play_harmonics(ma_device* pDevice, void* pOutput, const void* pInput, ma_ui
         double trigger = (g_amplitude * sin(2.0 * M_PI * (freq[0] + freq[0] * detune) * synth_time));
 
         for (int i = 0; i < harmonicsCount; i++) {
-            update_harmonics_amplitude(i);
+            update_parameter(harmonics_amplitude[i]);
 
-            value += g_amplitude * harmonics_amplitude[i] * sin(2.0 * M_PI * (freq[i] + freq[i] * detune) * (synth_time + phase * i));
+            value += sin(2.0 * M_PI * (freq[i] + freq[i] * detune) * (synth_time + phase * i)) * harmonics_amplitude[i].current_value * g_amplitude;
         }
 
         // left channel
@@ -164,35 +152,78 @@ int main() {
 
         switch (input)
         {
-            case '1': increase_harmonics_amplitude(0); break;
-            case '2': increase_harmonics_amplitude(1); break;
-            case '3': increase_harmonics_amplitude(2); break;
-            case '4': increase_harmonics_amplitude(3); break;
-            case '5': increase_harmonics_amplitude(4); break;
-            case '6': increase_harmonics_amplitude(5); break;
-            case '7': increase_harmonics_amplitude(6); break;
-            case '8': increase_harmonics_amplitude(7); break;
-            case '9': increase_harmonics_amplitude(8); break;
+        case '1':
+            increase_value(harmonics_amplitude[0]);
+            break;
+        case '2':
+            increase_value(harmonics_amplitude[1]);
+            break;
+        case '3':
+            increase_value(harmonics_amplitude[2]);
+            break;
+        case '4':
+            increase_value(harmonics_amplitude[3]);
+            break;
+        case '5':
+            increase_value(harmonics_amplitude[4]);
+            break;
+        case '6':
+            increase_value(harmonics_amplitude[5]);
+            break;
+        case '7':
+            increase_value(harmonics_amplitude[6]);
+            break;
+        case '8':
+            increase_value(harmonics_amplitude[7]);
+            break;
+        case '9':
+            increase_value(harmonics_amplitude[8]);
+            break;
 
-            case 'q': decrease_harmonics_amplitude(0); break;
-            case 'w': decrease_harmonics_amplitude(1); break;
-            case 'e': decrease_harmonics_amplitude(2); break;
-            case 'r': decrease_harmonics_amplitude(3); break;
-            case 't': decrease_harmonics_amplitude(4); break;
-            case 'y': decrease_harmonics_amplitude(5); break;
-            case 'u': decrease_harmonics_amplitude(6); break;
-            case 'i': decrease_harmonics_amplitude(7); break;
-            case 'o': decrease_harmonics_amplitude(8); break;
+        case 'q':
+            decrease_value(harmonics_amplitude[0]);
+            break;
+        case 'w':
+            decrease_value(harmonics_amplitude[1]);
+            break;
+        case 'e':
+            decrease_value(harmonics_amplitude[2]);
+            break;
+        case 'r':
+            decrease_value(harmonics_amplitude[3]);
+            break;
+        case 't':
+            decrease_value(harmonics_amplitude[4]);
+            break;
+        case 'y':
+            decrease_value(harmonics_amplitude[5]);
+            break;
+        case 'u':
+            decrease_value(harmonics_amplitude[6]);
+            break;
+        case 'i':
+            decrease_value(harmonics_amplitude[7]);
+            break;
+        case 'o':
+            decrease_value(harmonics_amplitude[8]);
+            break;
 
-            case 'a': phase += 0.00001; break;
-            case 'z': phase -= 0.00001; break;
-
-            case 's': detune += 0.01; break;
-            case 'x': detune -= 0.01; break;
+        case 'a':
+            phase += 0.00001;
+            break;
+        case 'z':
+            phase -= 0.00001;
+            break;
+        case 's':
+            detune += 0.01;
+            break;
+        case 'x':
+            detune -= 0.01;
+            break;
         }
 
         for (int i = 0; i < HARMONICS_COUNT; i++)
-            std::cout << (i+1) << " " << harmonics_amplitude[i] << std::endl;
+            std::cout << (i + 1) << " " << harmonics_amplitude[i].current_value << " " << harmonics_amplitude[i].target_value << std::endl;
 
         std::cout << std::endl;
         std::cout << "DETUNE: " << detune << std::endl;
