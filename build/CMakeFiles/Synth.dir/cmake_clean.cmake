2b0ecfef21b28c3968586d647604b9f29baa45c9
alex <r.a.cristian.93@gmail.com>
2023-11-25 13:33:27 +0200

    Added midi input

---
+++ build/CMakeFiles/Synth.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Synth.dir/include/RtMidi/RtMidi.cpp.o"
+  "CMakeFiles/Synth.dir/include/RtMidi/RtMidi.cpp.o.d"
+  "CMakeFiles/Synth.dir/include/nanomidi/msgprint.c.o"
+  "CMakeFiles/Synth.dir/include/nanomidi/msgprint.c.o.d"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_decoder.c.o"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_decoder.c.o.d"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_encoder.c.o"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_encoder.c.o.d"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_stream.c.o"
+  "CMakeFiles/Synth.dir/include/nanomidi/nanomidi_stream.c.o.d"
+  "CMakeFiles/Synth.dir/src/Synth.cpp.o"
+  "CMakeFiles/Synth.dir/src/Synth.cpp.o.d"
+  "Synth"
+  "Synth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Synth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
