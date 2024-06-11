2e0fa489fe78c3efb4a0739510852a0c52c5903d
r-a-cristian-93 <r.a.cristian.93@gmail.com>
2024-06-11 20:52:56 +0300

    inline organ oscillator function

--- src/OrganOscillator.cpp
+++ src/OrganOscillator.cpp
@@ -1,21 +1,15 @@
 #include "OrganOscillator.h"
 
-#include <iostream>
-#include <math.h>
-
 #include "Config.h"
-#include "Parameter.h"
-#include "WaveTables.h"
 #include "NoteFrequency.h"
 
-float phase_increment[MIDI_NOTES_COUNT][DRAWBARS_COUNT] = {{0}};
-
+float note_phase_increment[MIDI_NOTES_COUNT][DRAWBARS_COUNT] = {{0}};
 Parameter drawbar_amplitude[DRAWBARS_COUNT];
 
 void generate_phase_increment() {
     for (int midiNote = 0; midiNote < MIDI_NOTES_COUNT; midiNote++) {
         for (int drawbar_index = 0; drawbar_index < DRAWBARS_COUNT; drawbar_index++) {
-            phase_increment[midiNote][drawbar_index] =
+            note_phase_increment[midiNote][drawbar_index] =
                 note_frequency[midiNote][drawbar_index] * PHASE_LUT_SCALE_FACTOR ;
         }
     }
@@ -25,37 +19,6 @@ void organ_oscillator_init()
 {
     generate_phase_increment();
     EnvelopeAdsr_Init();
-}
-
-void organ_oscillator_update()
-{
-    for (int drawbar_index = 0; drawbar_index < DRAWBARS_COUNT; drawbar_index++)
-    {
-        drawbar_amplitude[drawbar_index].update();
-    }
-}
-
-double organ_oscillator_generate_sample(Note& note)
-{
-    double sample = 0;
-
-    for (int drawbar_index = 0; drawbar_index < DRAWBARS_COUNT; drawbar_index++)
-    {
-
-        sample += sine_table[(int)(note.phaseAccumulator[drawbar_index] )]
-            * drawbar_amplitude[drawbar_index].current_value * EnvelopeAdsr_GetAmplitude(&note.envelope)
-            * HEADROOM_SCALE_FACTOR;
-
-        note.phaseAccumulator[drawbar_index] += phase_increment[note.midiNote][drawbar_index];
-
-        if (note.phaseAccumulator[drawbar_index]  >= LUT_SIZE)
-            note.phaseAccumulator[drawbar_index]  -= LUT_SIZE;
-
-        if (note.phaseAccumulator[drawbar_index]  < 0)
-            note.phaseAccumulator[drawbar_index]  += LUT_SIZE;
-    }
-
-    return sample;
 }
 
 void organ_oscillator_set_drawbar_amplitude(int drawbar_index, float amplitude) {
--- src/OrganOscillator.h
+++ src/OrganOscillator.h
@@ -2,11 +2,45 @@
 #define ORGAN_OSCILATOR_H
 
 #include "Note.h"
+#include "Parameter.h"
+#include "WaveTables.h"
+
+extern float note_phase_increment[MIDI_NOTES_COUNT][DRAWBARS_COUNT];
+extern Parameter drawbar_amplitude[DRAWBARS_COUNT];
 
 void organ_oscillator_init();
-void organ_oscillator_update();
-double organ_oscillator_generate_sample(Note& note);
-
 void organ_oscillator_set_drawbar_amplitude(int drawbar, float amplitude);
+
+ __attribute__((always_inline)) inline
+void organ_oscillator_update()
+{
+    for (int drawbar_index = 0; drawbar_index < DRAWBARS_COUNT; drawbar_index++)
+    {
+        drawbar_amplitude[drawbar_index].update();
+    }
+}
+
+ __attribute__((always_inline)) inline
+double organ_oscillator_generate_sample(Note& note)
+{
+    double sample = 0;
+
+    for (int drawbar_index = 0; drawbar_index < DRAWBARS_COUNT; drawbar_index++)
+    {
+        sample += sine_table[(int)(note.phaseAccumulator[drawbar_index] )]
+            * drawbar_amplitude[drawbar_index].current_value * EnvelopeAdsr_GetAmplitude(&note.envelope)
+            * HEADROOM_SCALE_FACTOR;
+
+        note.phaseAccumulator[drawbar_index] += note_phase_increment[note.midiNote][drawbar_index];
+
+        if (note.phaseAccumulator[drawbar_index]  >= LUT_SIZE)
+            note.phaseAccumulator[drawbar_index]  -= LUT_SIZE;
+
+        if (note.phaseAccumulator[drawbar_index]  < 0)
+            note.phaseAccumulator[drawbar_index]  += LUT_SIZE;
+    }
+
+    return sample;
+}
 
 #endif
