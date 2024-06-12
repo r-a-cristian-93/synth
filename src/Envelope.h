86d72b6da4325e13567df52cc02a3f2651797766
r-a-cristian-93 <r.a.cristian.93@gmail.com>
2024-06-12 20:36:23 +0300

    evenelope

--- src/Envelope.h
+++ src/Envelope.h
@@ -29,8 +29,44 @@ struct EnvelopeSettings
 extern EnvelopeSettings envelope_settings;
 
 void envelope_initialize();
-float envelope_get_amplitude(Envelope *envelope);
 void envelope_note_on(Envelope *envelope);
 void envelope_note_off(Envelope *envelope);
+
+// Inline
+float envelope_get_amplitude(Envelope *envelope);
+
+
+__attribute__((always_inline)) inline
+float envelope_get_amplitude(Envelope* envelope)
+{
+    switch (envelope->state) {
+        case ADSR_IDLE:
+            break;
+        case ADSR_ATTACK:
+            envelope->amplitudeValue += envelope_settings.attackRate;
+            if (envelope->amplitudeValue >= MAX_AMPLITUDE) {
+                envelope->amplitudeValue = MAX_AMPLITUDE;
+                envelope->state = ADSR_DECAY;
+            }
+            break;
+        case ADSR_DECAY:
+            envelope->amplitudeValue -= envelope_settings.decayRate;
+            if (envelope->amplitudeValue <= envelope_settings.sustainLevel) {
+                envelope->amplitudeValue = envelope_settings.sustainLevel;
+                envelope->state = ADSR_SUSTAIN;
+            }
+            break;
+        case ADSR_SUSTAIN:
+            break;
+        case ADSR_RELEASE:
+            envelope->amplitudeValue -= envelope_settings.releaseRate;
+            if (envelope->amplitudeValue <= MIN_AMPLITUDE) {
+                envelope->amplitudeValue = MIN_AMPLITUDE;
+                envelope->state = ADSR_IDLE;
+            }
+    }
+
+    return envelope->amplitudeValue;
+}
 
 #endif
