3a1677b604c37d94c41f7d9ac0f9a3cb77d720e9
r-a-cristian-93 <r.a.cristian.93@gmail.com>
2024-06-20 21:40:53 +0300

    test embedded osc 1.056 sec (rotary still float)

---
+++ test/TestPerformance.cpp
@@ -0,0 +1,44 @@
+#include <OrganEngine/Config.h>
+#include <OrganEngine/OrganOscillator.h>
+#include <OrganEngine/RotarySpeaker.h>
+#include <OrganEngine/WaveTables.h>
+#include <OrganEngine/NoteManager.h>
+#include "../src/OrganSynthesizer.h"
+#include <iostream>
+#include "Profiler.h"
+#include <chrono>
+
+
+void testFunction() {
+
+}
+
+int main()
+{
+
+    waveforms_initialize();
+    organ_oscillator_initialize();
+    rotary_speaker_initialize();
+    note_manager_initialize();
+
+
+    const ma_uint32 frameCount = 10000000;
+    float* buffer = (float*) calloc(frameCount * 2, sizeof(float));
+
+    note_on(64);
+
+    using clock = std::chrono::high_resolution_clock;
+    auto start = clock::now();
+
+    dataCallback(0, buffer, 0, frameCount);
+
+    auto end = clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " seconds" << std::endl;
+
+    free(buffer);
+
+
+
+    return 0;
+}
